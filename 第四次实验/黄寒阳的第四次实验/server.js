c1d8b057369f55cad613972cab6805c368a650cd
hanyanghuang <897360868@qq.com>
2026-04-15 20:52:57 +0800

    黄寒阳完成第四次实验所有内容

--- 第四次实验/黄寒阳的第四次实验/server.js
+++ 第四次实验/黄寒阳的第四次实验/server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 
+app.listen(12399, () => {
+    console.log('服务器运行在 http://localhost:12399')
+})
+
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req,res) => {
-  res.render('hello', { dynamic_text: '新加入的动态内容' })
+
+app.get('/hello', (req, res) => {
+    res.render('hello', { dynamic_text: '欢迎光临黄寒阳的小站！' })
 })
 
-app.listen(12399)
+app.get('/', (req, res) => {
+    res.render('hello', { dynamic_text: '这是首页，很高兴遇见你！' })
+})
+
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/submit', (req, res) => {
+    let data = {
+        title: req.body.title,
+        description: req.body.description
+    }
+    res.render('display', { article: data })
+})
